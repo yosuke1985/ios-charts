caccc8cfd6807285ef471c4ac41c6abc1d6760a5
中山 陽介 <ihatenonoildressing@gmail.com>
2015-12-29 22:23:58 +0900

    initial commit

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+# use_frameworks!
+
+target 'iOS_Charts' do
+    pod 'Charts'
+
+use_frameworks!
+
+end
+
+target 'iOS_ChartsTests' do
+
+end
+
+target 'iOS_ChartsUITests' do
+
+end
+
---
+++ iOS_Charts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS_Charts
+//
+//  Created by 中山 陽介 on 12/29/15.
+//  Copyright © 2015 Yosuke Nakayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
